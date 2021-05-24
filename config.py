354a9b488acd9199a3e6323d76e825514e0b166c
Mike Vickers <mvickers@gcfb.org>
2021-05-23 21:16:12 -0400

    added first test and fixtures

---
+++ config.py
@@ -0,0 +1,10 @@
+
+#This is the local path to the cloud folder to be replaced entirely with sharepoint
+base_dir = "C:\\Users\\mvickers\\Gleaners Community Food Bank\\Healthcare Programs - Healthcare Working Directory"
+
+base_url = 'https://gcfbsm.sharepoint.com'
+dms_url = 'https://gcfbsm.sharepoint.com/sites/DMS'
+
+site_url = 'https://gcfbsm.sharepoint.com/sites/HealthcarePrograms'
+
+working_url = 'Shared Documents/General/Healthcare Working Directory'
